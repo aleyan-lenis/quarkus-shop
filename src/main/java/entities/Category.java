51204fc35173897ced9c5d8db1831c04afb841b6
lenisValmillo <bryan.vera@sasf.net>
2025-01-03 22:50:34 -0500

    Crear proyecto

---
+++ src/main/java/entities/Category.java
@@ -0,0 +1,45 @@
+package entities;
+
+import io.smallrye.common.constraint.NotNull;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.Objects;
+
+@Getter
+@NoArgsConstructor
+@ToString(callSuper = true)
+@Entity
+@Table(name = "categories")
+public class Category extends AbstractEntity {
+    @NotNull
+    @Column(name="name", nullable = false)
+    private String name;
+
+    @NotNull
+    @Column(name="description",nullable = false)
+    private String description;
+
+    public Category(@NotNull String name, @NotNull String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return Objects.equals(name, category.name) &&
+               Objects.equals(description, category.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, description);
+    }
+}
